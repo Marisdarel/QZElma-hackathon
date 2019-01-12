37a9dcc9241846d58dd94e4ed7906a8bdadb71f4
sergeev <sergeev@elewise.com>
2019-01-12 13:02:21 +0400

    Добавил чат бот сервайс

---
+++ QZElma-hackathon/ChatBotService/ChatBot.cs
@@ -0,0 +1,41 @@
+﻿using ChatBotService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace ChatBotService
+{
+    public class ChatBot
+    {
+        private readonly string token;
+        private readonly string hookUrl;
+        private TelegramBotClient client;
+        private readonly List<Command> commandList;
+
+        public ChatBot(string token, string hookUrl)
+        {
+            this.token = token;
+            this.hookUrl = hookUrl;
+            this.commandList = new List<Command>()
+            {
+                new HelloComand()
+            };
+        }
+
+        public async Task<TelegramBotClient> Get()
+        {
+            if(client != null)
+            {
+                return client;
+            }
+
+            client = new TelegramBotClient(token);
+            await client.SetWebhookAsync(hookUrl);
+
+            return client;
+        }
+
+        public List<Command> GetCommandList() => commandList;
+    }
+}
---
+++ QZElma-hackathon/ChatBotService/Commands/HelloComand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ChatBotService.Commands
+{
+    public class HelloComand : Command
+    {
+        public override string Name => "hellobot";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+            
+            await client.SendTextMessageAsync(0, "Hello", replyToMessageId: messageId);
+        }
+    }
+}
