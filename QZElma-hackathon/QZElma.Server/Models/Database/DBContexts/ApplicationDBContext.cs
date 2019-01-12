dfa72cb900bcc387b757d811b31168b414907d0f
Maksim Semenov <semenov@elewise.com>
2019-01-12 12:34:52 +0400

    Исправил инициализацию DBContext

--- QZElma-hackathon/QZElma.Server/Models/Database/DBContexts/ApplicationDBContext.cs
+++ QZElma-hackathon/QZElma.Server/Models/Database/DBContexts/ApplicationDBContext.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QZElma.Server.Models.Database.DBEntities;
+using System;
 
 
 namespace QZElma.Server.Models.Database.DBContexts
 {
     public class ApplicationDBContext : DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+            _guid = Guid.NewGuid();
+        }
+
+        private Guid _guid { get; set; }
+
+
         public DbSet<User> Users { get; set; }
 
         public DbSet<Room> Rooms { get; set; }
